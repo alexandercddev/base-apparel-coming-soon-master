e2559214c488c30b37ed67faff7b5a8e5550c782
Alexander Chi <arturochi2@hotmail.com>
2021-09-09 17:23:29 -0500

    Se agregó estructura de JSX Se agregó lógica de submit Se agregó hook para formulario y manejo de errores

--- src/App.js
+++ src/App.js
@@ -3,28 +3,39 @@
  * @description: 
  * @date: 08/septiembre/2021 
 **/
+import { useEffect, useState } from 'react';
 import './assets/scss/App.scss';
 import Image from './components/Image';
 import InputWithButton from './components/InputWithButton';
 import Paragraph from './components/Paragraph';
 import Title from './components/Title';
+import image from './assets/images/hero-desktop.jpg';
+import logo from './assets/svgs/logo.svg';
 
-const App = (props) => {
+const App = (props) => { 
     return (
-        <div className="card">
-            <div className="left"> 
-                <Title>
-                    We're
-                </Title>
-                <Paragraph>
-
-                </Paragraph>
-                <InputWithButton />
+        <section className="section">
+            <div className="col left"> 
+                <div className="col-content">
+                    <Image image={logo}/>
+                    <Title>
+                        We're coming soon
+                    </Title>
+                    <Paragraph>
+                        Hello fellow shoppers! We're currently building our new 
+                        fashion store. Add your email below to stay up-to-date with
+                        announcements and our launch deals.
+                    </Paragraph>
+                    <InputWithButton  
+                        name="email"  
+                        required={true} 
+                        placeholder="Email Address" />
+                </div>
             </div>
-            <div className="right">
-                <Image />
+            <div className="col right">
+                <Image image={image}/>
             </div> 
-        </div>
+        </section>
     );
  }
  
--- src/components/InputWithButton.js
+++ src/components/InputWithButton.js
@@ -4,13 +4,51 @@
  * @date: 08/septiembre/2021 
 **/
 import React, { useState } from 'react';  
+import { useForm } from 'react-hook-form';
 
 const InputWithButton = (props) => {
-    const { image, name } = props
+    const { placeholder, name, type, required, message } = props
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        alert(`Send email ${data.email}`);   
+    }
     return (
         <div className="input-with-button">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input 
+                    placeholder={placeholder} 
+                    className="input" 
+                    type={type && 'text'} 
+                    message={message}
+                    {...register(name, { 
+                        required: {
+                            value: required, 
+                            message: "This is required"
+                        },
+                        pattern: {
+                            value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i, 
+                            message: "This is not format"
+                        }
+                    })}/> 
+                <input 
+                    className="btn arrow" 
+                    type="submit" value="Submit"/>
+            </form> 
+            <Error errors={errors} validate="required" name={name}/>
+            <Error errors={errors} validate="pattern" name={name}/> 
         </div>
     );
+} 
+
+const Error = (props) => {
+    const { errors, validate, name } = props;
+    return (
+        errors?.[name] && errors?.[name].type === validate ? ( 
+            <span className="error">{errors?.[name].message}</span>
+        ) : (
+            null
+        )
+    )
 }
 
 export default InputWithButton;
